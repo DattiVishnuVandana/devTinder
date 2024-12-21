dc90a91504d5f833dcc9b757ef3ffc4ec39822c6
vandana_datti <vandanadatti567@gmail.com>
2024-12-21 12:53:32 +0530

    routes,regex

--- src/app.js
+++ src/app.js
@@ -2,20 +2,37 @@
 const express=require("express"                                                                                                                   );
 const app=express()
 
-app.get("/user",(req,res)=>{
+app.get("/a(bk)+c",(req,res)=>{
+    res.send("regex")
+})//*,?,+,(.*..$,/a/
+app.get(/a/,(req,res)=>{
+    res.send("regex2")
+})
+app.get(/.*fly$/,(req,res)=>{
+    res.send("regex3")
+})
+app.get("/user/:userid/:uname/:pass",(req,res)=>{
+    console.log(req.params);
     res.send({firstname:"vandana",lastname:"datti"})
 })
-app.post("/user",(req,res)=>{
-    res.send("data succesfully posted")
-})
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);
+//     res.send({firstname:"vandana",lastname:"datti"})
+// })
+// app.get("/user",(req,res)=>{
+//     res.send({firstname:"vandana",lastname:"datti"})
+// })
+// app.post("/user",(req,res)=>{
+//     res.send("data succesfully posted")
+// })
 
-app.delete("/user",(req,res)=>{
-    res.send("deleted succesfully...")
-})
+// app.delete("/user",(req,res)=>{
+//     res.send("deleted succesfully...")
+// })
 
-app.use("/user",(req,res)=>{
-    res.send("succesfully done api ..handles all calls")
-})
+// app.use("/user",(req,res)=>{
+//     res.send("succesfully done api ..handles all calls")
+// })
 
 app.listen(7777,()=>{
     console.log("server is succesfully running in port 7777..");
