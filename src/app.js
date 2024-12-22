53cd690f4e99714d5cf0ccf9d720181538460221
vandana_datti <vandanadatti567@gmail.com>
2024-12-22 22:34:50 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,39 +1,44 @@
 // console.log("1 st time creating server...");
 const express=require("express"                                                                                                                   );
 const app=express()
-
-app.get("/a(bk)+c",(req,res)=>{
-    res.send("regex")
-})//*,?,+,(.*..$,/a/
-app.get(/a/,(req,res)=>{
-    res.send("regex2")
-})
-app.get(/.*fly$/,(req,res)=>{
-    res.send("regex3")
-})
-app.get("/user/:userid/:uname/:pass",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"vandana",lastname:"datti"})
-})
-// app.get("/user",(req,res)=>{
-//     console.log(req.query);
-//     res.send({firstname:"vandana",lastname:"datti"})
-// })
-// app.get("/user",(req,res)=>{
-//     res.send({firstname:"vandana",lastname:"datti"})
-// })
-// app.post("/user",(req,res)=>{
-//     res.send("data succesfully posted")
-// })
-
-// app.delete("/user",(req,res)=>{
-//     res.send("deleted succesfully...")
-// })
-
 // app.use("/user",(req,res)=>{
-//     res.send("succesfully done api ..handles all calls")
+//     console.log("route handler");
+//     // res.send("")
 // })
+// app.use("/user",(req,res,next)=>{
 
+//     console.log("route handler");
+//     res.send("1")
+//     // next()
+//     console.log("termination");
+    
+// },(req,res,next)=>{
+//     console.log("route handler2");
+//     // res.send("2")
+// })
+app.use("/user",[[(req,res,next)=>{
+
+    console.log("route handler1");
+    // res.send("1")
+    next()
+    console.log("termination1");
+    
+},(req,res,next)=>{
+
+    console.log("route handler2");
+    // res.send("1")
+    next()
+    console.log("termination2");
+    
+}],
+(req,res,next)=>{
+
+    console.log("route handler3");
+    res.send("3")
+    // next()
+    console.log("termination3");
+    
+}])
 app.listen(7777,()=>{
     console.log("server is succesfully running in port 7777..");
 })
