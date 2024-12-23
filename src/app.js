b0c4504fbb2d3fef11e84af619e57869cda498c3
vandana_datti <vandanadatti567@gmail.com>
2024-12-23 06:47:51 +0530

    middleware demo

--- src/app.js
+++ src/app.js
@@ -1,22 +1,14 @@
 // console.log("1 st time creating server...");
 const express=require("express"                                                                                                                   );
 const app=express()
-// app.use("/user",(req,res)=>{
-//     console.log("route handler");
-//     // res.send("")
-// })
-// app.use("/user",(req,res,next)=>{
 
-//     console.log("route handler");
-//     res.send("1")
-//     // next()
-//     console.log("termination");
-    
-// },(req,res,next)=>{
-//     console.log("route handler2");
-//     // res.send("2")
-// })
-app.use("/user",[[(req,res,next)=>{
+app.use("/",(req,res,next)=>{
+console.log("passing");
+// res.send("0")
+next()
+console.log("done 0");
+})
+app.get("/user",[[(req,res,next)=>{
 
     console.log("route handler1");
     // res.send("1")
