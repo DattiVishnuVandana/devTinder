3611f5d9d117c05cef18235bacc05ba6fcbadcc9
vandana_datti <vandanadatti567@gmail.com>
2024-12-23 08:45:12 +0530

    wildcard-trycatch error handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,21 @@
 
 const express=require("express") ;      
-
-const {adminAuth,userAuth}=require("./auth") ;
-                                                                                               
 const app=express()
-app.use("/admin",adminAuth)
-app.use("/admin/getData",(req,res)=>{
-    res.send("send data")
-})
-app.use("/user/login",(req,res,next)=>{
-    console.log("1st one");
-    res.send("user is authenticated login")
-    // next()
-})
 
-app.use("/user/data",userAuth,(req,res,next)=>{
-    console.log("2nd one");
-    res.send("user is authenticated")
+app.use("/user/login",(req,res)=>{
+    try{
+    throw new Error("rthg");
+    res.send("done")
+    }catch(err){
+        res.status(500).send("something wrong in  u r code")
+    }
 })
+// app.use("/",(err,req,res,next)=>{
+//     if(err){
+//         res.status(500).send("something went wrong...")
+//     }
+// })
 
-app.use("/admin/delete",(req,res)=>{
-    res.send("deleted data")
-})
 app.listen(7777,()=>{
     console.log("server is succesfully running in port 7777..");
 })
